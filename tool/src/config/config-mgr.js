3771bdcb0b230d58f64d12dd1d7f0febed915b5e
MarucoMass <mariomass23@gmail.com>
2025-01-16 21:47:11 -0300

    added debug package

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -1,27 +1,30 @@
 // import { pkgUpSync } from "pkg-up";
 // import { readFileSync } from "fs";
+import { createLogger } from '../logger.js';
 import chalk from 'chalk';
 import { cosmiconfigSync } from "cosmiconfig";
-const configLoader = cosmiconfigSync("tool");
 import schema from './schema.json' assert { type: "json" };
 import betterAjvErrors from "better-ajv-errors";
 import Ajv from 'ajv';
+
+const configLoader = cosmiconfigSync("tool");
+const logger = createLogger('config:mgr');
 const ajv = new Ajv();
 
 export function getConfig() {
   const result = configLoader.search(process.cwd());
   if (!result) {
-    console.log(chalk.yellow("Could not find configuration, using default"));
+    logger.warning('Could not find configuration, using default');
     return { port: 1234 };
   } else {
     const isValid = ajv.validate(schema, result.config);
     if (!isValid) {
-      console.log(chalk.yellow('Invalid configuration was supplied'));
+      logger.warning(chalk.yellow('Invalid configuration was supplied'));
       console.log();
       console.log(betterAjvErrors(schema, result.config, ajv.errors));
       process.exit(1);
     }
-    console.log("Found configuration", result.config);
+    logger.debug("Found configuration", result.config);
     return result.config;
   }
   //   const pkgPath = pkgUpSync({ cwd: process.cwd() });
